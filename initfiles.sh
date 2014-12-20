79b3c4458962b8f02ec0f210617418ab8a1ee286
c01o <melty.antarcticite@gmail.com>
2014-12-20 20:50:50 +0900

    automate dotfiles sym-link
    ~/dotfiles/initfiles.sh is the right position.
    use `git clone https://github.com/c01o/dotfiles` in ~/

---
+++ initfiles.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# current dir
+cd $(dirname $0)
+
+# make symlink
+for dotfiles in .?*; do
+    case $dotfiles in 
+        ..)
+            continue;;
+        .git)
+            continue;;
+        *)
+            ln -Fis "$PWD/$dotfiles" $HOME;;
+    esac
+done
+
+echo "\ncompleted!"
